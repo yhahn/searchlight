b745cc679e7146551865117bae2b23dc1407c0fc
Adrian Rossouw <adrian@developmentseed.org>
2010-05-11 12:32:08 -0400

    fix variable names

--- theme/searchlight-sphinx-index-conf.tpl.php
+++ theme/searchlight-sphinx-index-conf.tpl.php
@@ -1,40 +1,40 @@
 #############################################################################
-# datasource: <?php print $id ?>
+# datasource: <?php print $datasource['conf']['id'] ?>
 #############################################################################
 
-source <?php print $id ?>
+source <?php print $datasource['conf']['id'] ?>
 {
-type = <?php print $type ?>
-sql_host = <?php print $sql_host ?>
-sql_user = <?php print $sql_user ?>
-sql_pass = <?php print $sql_pass ?>
-sql_db   = <?php print $sql_db ?>
-sql_port = <?php print $sql_port ?>
-<?php if (!empty($sql_sock )): ?>
-sql_sock = <?php print $sql_sock ?>
+type = <?php print $datasource['conf']['type'] ?>
+sql_host = <?php print $datasource['conf']['sql_host'] ?>
+sql_user = <?php print $datasource['conf']['sql_user'] ?>
+sql_pass = <?php print $datasource['conf']['sql_pass'] ?>
+sql_db   = <?php print $datasource['conf']['sql_db'] ?>
+sql_port = <?php print $datasource['conf']['sql_port'] ?>
+<?php if (!empty($datasource['conf']['sql_sock'] )): ?>
+sql_sock = <?php print $datasource['conf']['sql_sock'] ?>
 <?php endif; ?>
 
 sql_query = \
-  <?php print $sql_query ?>
+  <?php print $datasource['conf']['sql_query'] ?>
 sql_query_info = \
-  <?php print $sql_query_info ?>
-sql_query_range = <?php print $sql_query_range ?>
-sql_range_step = <?php print $sql_range_step ?>
-sql_ranged_throttle = <?php print $sql_ranged_throttle ?>
+  <?php print $datasource['conf']['sql_query_info'] ?>
+sql_query_range = <?php print $datasource['conf']['sql_query_range'] ?>
+sql_range_step = <?php print $datasource['conf']['sql_range_step'] ?>
+sql_ranged_throttle = <?php print $datasource['conf']['sql_ranged_throttle'] ?>
 
-<?php print $sql_attr ?>
+<?php print $datasource['conf']['sql_attr'] ?>
 }
 
 #############################################################################
-# index: <?php print $id ?>
+# index: <?php print $datasource['conf']['id'] ?>
 #############################################################################
 
-index <?php print $id ?>
+index <?php print $datasource['conf']['id'] ?>
 {
 # Index configuration
-source = <?php print $id ?>
-<?php foreach ($index as $key => $value) :  ?>
-  <?php print $key ?> = <?php print $index[$key] ?>
+source = <?php print $datasource['conf']['id'] ?>
+<?php foreach ($datasource['index'] as $key => $value) :  ?>
+  <?php print $key ?> = <?php print $datasource['index'][$key] ?>
 <?php endforeach; ?>
 } 
 
