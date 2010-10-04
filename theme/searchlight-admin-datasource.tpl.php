a3707f29c918921d0aac8cf8d5b45578cebce5e2
Adrian Rossouw <adrian@developmentseed.org>
2010-10-05 01:49:40 +0200

    got all the forms displaying, but not working yet. got all the js to work.

--- theme/searchlight-admin-datasource.tpl.php
+++ theme/searchlight-admin-datasource.tpl.php
@@ -18,4 +18,5 @@
   <?php print drupal_render($form['fields']) ?>
   <?php print drupal_render($form['multivalues']) ?>
   <?php print drupal_render($form['options']) ?>
+  <?php print drupal_render_children($form) ?>
 </div>
